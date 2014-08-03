dbea592cab4150bbcf0e342e6ee34a0eaf636b26
Takamura Soichi <takamura_s@ariel-networks.com>
2014-08-03 12:02:02 +0000

    use ejs and linker

--- api/services/webdriver.js
+++ api/services/webdriver.js
@@ -49,27 +49,6 @@ Executor.prototype.execute = function() {
 
 
 
-// Example
-var seed = {
-	title: 'xxx',
-	entries: [
-		{title: 'open website', mode: 'action', type: 'open', text: 'http://yan-yan-yahuoku.com'},
-		{title: 'remove extra tag', mode: 'action', type: 'click', css: '.button-tag .button-remove'},
-		{title: 'input search text', mode: 'action', type: 'input', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.tag-input div.tag-input-leftcontent:nth-child(2) input.tag-input-textbox', text: 'AKB' + webdriver.Key.ENTER},
-		{title: 'verify whether any item was loaded', mode: 'verify', type: 'contains', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.goog-splitpane:nth-child(2) div.goog-splitpane-first-container div.goog-list.pane-list.goog-splitpane-first-container div.goog-list-container:nth-child(2) div.pure-g.pane-list-item.goog-list-item div.pure-u-18-24:nth-child(2) div.pane-list-item-title', text: 'AKB'}
-	]
-};
-var executor = new Executor(seed.entries);
-executor.on('before', function (entry) { console.log('before ' + entry.title + ' ...') })
-executor.on('pass', function () { console.log('test passed.') })
-executor.on('error', function (e) { console.log(e, 'error occures!!!!!!!') })
-executor.on('end', function () { console.log('end.') })
-executor.execute();
-
-
-
-
-
 /**
  * executionMap[mode][type](css, text)
  */
