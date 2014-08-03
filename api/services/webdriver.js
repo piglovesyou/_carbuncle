a7e01b61154f48c941fc94774e118f21bf1df310
Takamura Soichi <takamura_s@ariel-networks.com>
2014-08-03 04:17:46 +0000

    executor

--- api/services/webdriver.js
+++ api/services/webdriver.js
@@ -3,12 +3,73 @@ var webdriver = require('selenium-webdriver');
 global.goog = require('closure').Closure({ CLOSURE_BASE_PATH: 'libs/closure-library/closure/goog/' });
 goog.require('goog.string');
 var Q = require('q');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
 
 
-var context = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();
+module.exports.Executor = Executor;
+var seed = {
+	title: 'xxx',
+	entries: [
+		{title: '', mode: 'action', type: 'open', text: 'http://yan-yan-yahuoku.com'},
+		{title: '', mode: 'action', type: 'click', css: '.button-tag .button-remove'},
+		{title: '', mode: 'action', type: 'input', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.tag-input div.tag-input-leftcontent:nth-child(2) input.tag-input-textbox', text: 'AKB' + webdriver.Key.ENTER},
+		{title: '', mode: 'verify', type: 'contains', css: 'body:nth-child(2) iv.body:nth-child(2) div.frame.app-frame-selected div.goog-splitpane:nth-child(2) div.goog-splitpane-first-container div.goog-list.pane-list.goog-splitpane-first-container div.goog-list-container:nth-child(2) div.pure-g.pane-list-item.goog-list-item div.pure-u-18-24:nth-child(2) div.pane-list-item-title', text: 'AKB'}
+	]
+};
+
+
+
+function Executor(entries) {
+  EventEmitter.call(this) 
+  Object.defineProperty(this, 'entries', { value: entries })
+  Object.seal(this);
+};
+util.inherits(Executor, EventEmitter);
+
+Executor.prototype.execute = function() {
+  var context = new Context;
+
+  return this.entries.reduce(function(p, e) {
+    return p.then(function() {
+      return executionMap[e.mode][e.type](context, e.css, e.text);
+    });
+  }, Q())
+  .catch (function(e) {
+    context.quit(); // Make sure to quit even if an error occures.
+    throw new Error(e);
+  })
+  .then(function(canbeError) {
+    context.quit();
+    return context;
+  });
+};
+
+var executor = new Executor(seed.entries);
+executor.execute()
+.then(function(err) {
+  console.log(err.stack);
+})
+.catch(function(e) {
+  console.log(e.stack);
+})
+
+
+
+
+
+// execute(seed.entries)
+// .then(function(err) {
+//   console.log(err.stack);
+// })
+// .catch(function(e) {
+//   console.log(e.stack);
+// })
+
+
+
+
 
 
 /**
@@ -30,49 +91,29 @@ var executionMap = {
 
 
 
-var seed = {
-	title: 'xxx',
-	entries: [
-		{mode: 'action', type: 'open', text: 'http://yan-yan-yahuoku.com'},
-		{mode: 'action', type: 'click', css: '.button-tag .button-remove'},
-		{mode: 'action', type: 'input', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.tag-input div.tag-input-leftcontent:nth-child(2) input.tag-input-textbox', text: 'AKB' + webdriver.Key.ENTER},
-		{mode: 'verify', type: 'contains', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.goog-splitpane:nth-child(2) div.goog-splitpane-first-container div.goog-list.pane-list.goog-splitpane-first-container div.goog-list-container:nth-child(2) div.pure-g.pane-list-item.goog-list-item div.pure-u-18-24:nth-child(2) div.pane-list-item-title', text: 'AKB'}
-	]
-};
 
 
 
-seed.entries.reduce(function(p, e) {
-	return p.then(function() {
-		return executionMap[e.mode][e.type](e.css, e.text);
-	});
-}, Q())
-.catch (function(e) {
-	console.log(e.stack);
-})
-.done(function() {
-	context.quit();
-});
 
-
-
-function actionOpen(noUse, url) {
-	return context.get(url);
+function actionOpen(context, noUse, url) {
+	return context.open(url)
 }
-function actionClick(css) {
-	return waitElemnet(css).then(function() {
-		return context.findElement(webdriver.By.css(css)).click();
+function actionClick(context, css) {
+	return context.wait(css).then(function() {
+    return context.find(css).click().then(function() {
+		  return context;
+		})
+	})
+}
+function actionInput(context, css, input) {
+	return context.wait(css).then(function() {
+		return context.find(css).sendKeys(input);
 	});
 }
-function actionInput(css, input) {
-	return waitElemnet(css).then(function() {
-		return context.findElement(webdriver.By.css(css)).sendKeys(input);
-	});
-}
-function verify(type, css, text) {
+function verify(type, context, css, text) {
 	var method = mapVerifyMethod(type);
-	return waitElemnet(css).then(function() {
-		return context.findElement(webdriver.By.css(css)).getText().then(function(v) {
+	return context.wait(css).then(function() {
+		return context.find(css).getText().then(function(v) {
 			assert(method(v, text));
 		});
 	});
@@ -80,6 +121,35 @@ function verify(type, css, text) {
 
 
 
+
+function Context() {
+  Object.defineProperty(this, 'driver_', {
+    value: new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build()
+  })
+  Object.seal(this);
+}
+Context.prototype.open = function (url) {
+  return this.driver_.get(url);
+}
+Context.prototype.wait = function (selector) {
+  var that = this;
+	return that.driver_.wait(function() {
+		return that.driver_.isElementPresent(webdriver.By.css(selector));
+	}, 1000);
+}
+Context.prototype.find = function (css) {
+  return this.driver_.findElement(webdriver.By.css(css));
+}
+Context.prototype.quit = function () {
+  return this.driver_.quit();
+}
+
+
+
+
+// static tools
 function mapVerifyMethod(type) {
 	assert([
 		'equal',
@@ -89,9 +159,4 @@ function mapVerifyMethod(type) {
 	].some(function(s) {return s == type}));
 	return type == 'equal' ?
 		function(text, v) { return text == v } : goog.string[type];
-}
-function waitElemnet(selector) {
-	return context.wait(function() {
-		return context.isElementPresent(webdriver.By.css(selector));
-	}, 1000);
 }
--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+
+global.goog = require('closure').Closure({ CLOSURE_BASE_PATH: 'libs/closure-library/closure/goog/' });
+goog.require('goog.string');
+
+global.Q = require('q');
+
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
+
