96284857b92c551469c09398af767ecc3e8bb3a5
Takamura Soichi <takamura_s@ariel-networks.com>
2014-08-03 04:33:34 +0000

    EventEmitter version

--- api/services/webdriver.js
+++ api/services/webdriver.js
@@ -12,61 +12,56 @@ module.exports.Executor = Executor;
 var seed = {
 	title: 'xxx',
 	entries: [
-		{title: '', mode: 'action', type: 'open', text: 'http://yan-yan-yahuoku.com'},
-		{title: '', mode: 'action', type: 'click', css: '.button-tag .button-remove'},
-		{title: '', mode: 'action', type: 'input', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.tag-input div.tag-input-leftcontent:nth-child(2) input.tag-input-textbox', text: 'AKB' + webdriver.Key.ENTER},
-		{title: '', mode: 'verify', type: 'contains', css: 'body:nth-child(2) iv.body:nth-child(2) div.frame.app-frame-selected div.goog-splitpane:nth-child(2) div.goog-splitpane-first-container div.goog-list.pane-list.goog-splitpane-first-container div.goog-list-container:nth-child(2) div.pure-g.pane-list-item.goog-list-item div.pure-u-18-24:nth-child(2) div.pane-list-item-title', text: 'AKB'}
+		{title: 'open website', mode: 'action', type: 'open', text: 'http://yan-yan-yahuoku.com'},
+		{title: 'remove extra tag', mode: 'action', type: 'click', css: '.button-tag .button-remove'},
+		{title: 'input search text', mode: 'action', type: 'input', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.tag-input div.tag-input-leftcontent:nth-child(2) input.tag-input-textbox', text: 'AKB' + webdriver.Key.ENTER},
+		{title: 'verify whether any item was loaded', mode: 'verify', type: 'contains', css: 'body:nth-child(2) div.body:nth-child(2) div.frame.app-frame-selected div.goog-splitpane:nth-child(2) div.goog-splitpane-first-container div.goog-list.pane-list.goog-splitpane-first-container div.goog-list-container:nth-child(2) div.pure-g.pane-list-item.goog-list-item div.pure-u-18-24:nth-child(2) div.pane-list-item-title', text: 'AKB'}
 	]
 };
 
 
 
 function Executor(entries) {
-  EventEmitter.call(this) 
-  Object.defineProperty(this, 'entries', { value: entries })
+  EventEmitter.call(this);
+  Object.defineProperty(this, 'entries', { value: entries });
   Object.seal(this);
-};
+}
 util.inherits(Executor, EventEmitter);
 
 Executor.prototype.execute = function() {
+  var that = this;
   var context = new Context;
-
-  return this.entries.reduce(function(p, e) {
+  // Not return promise for preventing confusing
+  this.entries.reduce(function(p, e) {
     return p.then(function() {
+      that.emit('before', e);
       return executionMap[e.mode][e.type](context, e.css, e.text);
     });
   }, Q())
-  .catch (function(e) {
-    context.quit(); // Make sure to quit even if an error occures.
-    throw new Error(e);
-  })
-  .then(function(canbeError) {
+  .then(function() {
+    that.emit('pass');
     context.quit();
+    that.emit('end');
     return context;
-  });
+  })
+  .catch (function(e) {
+    that.emit('error', e);
+    context.quit(); // Make sure to quit even if an error occures.
+    that.emit('end', e);
+    // throw new Error(e);
+  })
 };
 
+
+
+
+// Example
 var executor = new Executor(seed.entries);
-executor.execute()
-.then(function(err) {
-  console.log(err.stack);
-})
-.catch(function(e) {
-  console.log(e.stack);
-})
-
-
-
-
-
-// execute(seed.entries)
-// .then(function(err) {
-//   console.log(err.stack);
-// })
-// .catch(function(e) {
-//   console.log(e.stack);
-// })
-
+executor.on('before', function (entry) { console.log('before ' + entry.title + ' ...') })
+executor.on('pass', function () { console.log('test passed.') })
+executor.on('error', function (e) { console.log(e, 'error occures!!!!!!!') })
+executor.on('end', function () { console.log('end.') })
+executor.execute();
 
 
 
@@ -96,14 +91,14 @@ var executionMap = {
 
 
 function actionOpen(context, noUse, url) {
-	return context.open(url)
+	return context.open(url);
 }
 function actionClick(context, css) {
 	return context.wait(css).then(function() {
     return context.find(css).click().then(function() {
 		  return context;
-		})
-	})
+		});
+	});
 }
 function actionInput(context, css, input) {
 	return context.wait(css).then(function() {
@@ -127,24 +122,24 @@ function Context() {
     value: new webdriver.Builder().
         withCapabilities(webdriver.Capabilities.chrome()).
         build()
-  })
+  });
   Object.seal(this);
 }
-Context.prototype.open = function (url) {
+Context.prototype.open = function(url) {
   return this.driver_.get(url);
-}
-Context.prototype.wait = function (selector) {
+};
+Context.prototype.wait = function(selector) {
   var that = this;
 	return that.driver_.wait(function() {
 		return that.driver_.isElementPresent(webdriver.By.css(selector));
 	}, 1000);
-}
-Context.prototype.find = function (css) {
+};
+Context.prototype.find = function(css) {
   return this.driver_.findElement(webdriver.By.css(css));
-}
-Context.prototype.quit = function () {
+};
+Context.prototype.quit = function() {
   return this.driver_.quit();
-}
+};
 
 
 
