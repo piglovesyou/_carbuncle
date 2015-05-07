161f2fc61958f2e005f37db8fddc0eba1d0efa37
piglovesyou <thepiglovesyou@gmail.com>
2015-05-08 00:50:20 +0900

    refac in progress

--- src/stores/SettingState.js
+++ src/stores/SettingState.js
@@ -3,55 +3,59 @@ var Dispatcher = require('../dispatcher');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 var Auth = require('../persist/auth');
-// var Q = require('q');
 var _ = require('underscore');
 var {CHANGE_EVENT, MongoErrorCode} = require('../constants');
 
-
-
-var store_ = {
+var default_ = {
   databaseConnected: null,
   authenticated: null
 };
 
-var SettingState = assign({}, EventEmitter.prototype, {
-  get() {
-    return store_;
-  },
-  authenticate
-}, require('./mixins_'));
-module.exports = SettingState;
 
 
-
-SettingState.dispatcherToken = Dispatcher.register(function(action) {
-  switch (action.type) {
-    case 'changeSetting':
-      _.extend(window.localStorage, action.form);
-      authenticate();
-      break;
+class SettingState extends EventEmitter {
+  constructor() {
+    super();
+    this.store_ = _.clone(default_);
+    this.dispatcherToken = Dispatcher.register(this.dispatcherHandler_.bind(this));
   }
-});
-
-
-
-function authenticate() {
-  Auth.authenticate()
-  .then(() => {
-    store_.databaseConnected = true;
-    store_.authenticated = true;
-    return SettingState.emit(CHANGE_EVENT);
-  })
-  .catch(err => {
-    if (!err.code) {
-      store_.databaseConnected = false;
-      store_.authenticated = false;
-      return SettingState.emit(CHANGE_EVENT);
-    } else if (err.code === MongoErrorCode.AUTH_FAILED) {
-      store_.databaseConnected = true;
-      store_.authenticated = false;
-      return SettingState.emit(CHANGE_EVENT);
+  get() {
+    return this.store_;
+  }
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+  dispatcherHandler_(action) {
+    switch (action.type) {
+      case 'changeSetting':
+        _.extend(window.localStorage, action.form);
+        authenticate();
+        break;
     }
-    throw new Error('catch this!', err);
-  });
+  }
+  authenticate() {
+    Auth.authenticate()
+    .then(() => {
+      this.store_.databaseConnected = true;
+      this.store_.authenticated = true;
+      return this.emit(CHANGE_EVENT);
+    })
+    .catch(err => {
+      if (!err.code) {
+        this.store_.databaseConnected = false;
+        this.store_.authenticated = false;
+        return this.emit(CHANGE_EVENT);
+      } else if (err.code === MongoErrorCode.AUTH_FAILED) {
+        this.store_.databaseConnected = true;
+        this.store_.authenticated = false;
+        return this.emit(CHANGE_EVENT);
+      }
+      throw new Error('catch this!', err);
+    });
+  }
 }
+
+module.exports = new SettingState();
