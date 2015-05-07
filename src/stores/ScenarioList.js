12feb8310c70839fc5f89133a0cb3ab1e50355f6
piglovesyou <thepiglovesyou@gmail.com>
2015-05-07 09:02:36 +0900

    refactor: "new ScenarioList" to be able to generate multiple instances

--- src/stores/ScenarioList.js
+++ src/stores/ScenarioList.js
@@ -8,65 +8,68 @@ var Persist = require('../persist');
 
 
 
-var _store;
-var _default = {
+var DEFAULT_STORE = {
   total: -1,
   list: [],
   map: []
 };
-restoreCache();
 
-var ScenarioList = assign({}, EventEmitter.prototype, {
+class ScenarioList extends EventEmitter {
+  constructor() {
+    super();
+    this.restoreCache();
+    this.dispatcherToken = Dispatcher.register(this._dispatcherHandler.bind(this));
+  }
+  _dispatcherHandler(action) {
+    switch (action.type) {
+      case 'saveScenario':
+        this.restoreCache();
+        break;
+      case 'deleteScenario':
+        // TODO: Update total
+        var {scenario} = action;
+        Persist.deleteScenario(scenario._id)
+        .then(() => {
+          if (goog.array.removeIf(this._store.list, s => String(s._id) === String(scenario._id))) {
+            delete this._store.map[scenario._id];
+            this._store.total = Math.max(this._store.total - 1, 0);
+            this.emit(CHANGE_EVENT);
+          }
+        });
+        break;
+    }
+  }
   get() {
-    return _.clone(_store);
-  },
-
+    return _.clone(this._store);
+  }
   sync(page) {
     var skip = PER_PAGE * page;
-    var limit = _store.total < 0 ? PER_PAGE : Math.min(_store.total - skip, PER_PAGE);
+    var limit = this._store.total < 0 ? PER_PAGE : Math.min(this._store.total - skip, PER_PAGE);
     var range = _.range(skip, skip + limit);
-    if (_store.total >= 0 &&
+    if (this._store.total >= 0 &&
         range.every(i =>
-          !!_store.list[i] &&
-          !_store.list[i]['@expired'])) {
+          !!this._store.list[i] &&
+          !this._store.list[i]['@expired'])) {
       return;
     }
     return Persist.getScenarios(page)
     .then(data => {
-      _store.total = data.total;
+      this._store.total = data.total;
       data.docs.forEach((doc, i) => {
-        _store.map[doc._id] = _store.list[data.skip + i] = doc;
+        this._store.map[doc._id] = this._store.list[data.skip + i] = doc;
       });
-      ScenarioList.emit(CHANGE_EVENT);
+      this.emit(CHANGE_EVENT);
     });
   }
-}, require('./_mixins'));
-module.exports = ScenarioList;
-
-ScenarioList.dispatcherToken = Dispatcher.register(function(action) {
-  switch (action.type) {
-    case 'saveScenario':
-      restoreCache();
-      break;
-
-    case 'deleteScenario':
-      // TODO: Update total
-      var {scenario} = action;
-      Persist.deleteScenario(scenario._id)
-      .then(() => {
-        if (goog.array.removeIf(_store.list, s => String(s._id) === String(scenario._id))) {
-          delete _store.map[scenario._id];
-          _store.total = Math.max(_store.total - 1, 0);
-          ScenarioList.emit(CHANGE_EVENT);
-        }
-      });
-      break;
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
   }
-});
-
-
-
-function restoreCache() {
-  _store = {};
-  _.each(_default, (v, k) => _store[k] = _.clone(v));
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+  restoreCache() {
+    this._store = _.clone(DEFAULT_STORE);
+  }
 }
+
+module.exports = new ScenarioList;
